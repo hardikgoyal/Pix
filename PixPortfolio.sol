bcb578fa7b5c9f5829a30d7890c5c66c5440e754
Hyuck Ju Kim <hyuckjuk@usc.edu>
2018-12-03 12:32:41 -0800

    added comments for PixToken, updated PixPortfolio

--- PixPortfolio.sol
+++ PixPortfolio.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.24;
 
 // Import the custom token contract you just wrote.
 import "./PixToken.sol";
+// Import OpenZeppelin's Ownable contract.
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /** This is an assignment to create a smart contract that allows you to run your own token crowdsale.
  *  Your contract will mint your custom token every time a purchase is made by your or your classmates.
@@ -12,30 +14,26 @@ import "./PixToken.sol";
  */
  
 // Set up your contract.
-contract MintedCrowdsale {
+contract MintedCrowdsale is Ownable {
     // Attach SafeMath library functions to the uint256 type.
     using SafeMath for uint256;
 
-    // Define 4 publicly accessible state variables. 
-    // Your custom token being sold.
-    PixToken public token;
+    // Array to save all the tokens
+    PixToken[] public token;
   
-    // Wallet address where funds are collected.
+    // Owner's wallet
     address public wallet;
 
-    // Rate of how many token units a buyer gets per wei. Note that wei*10^-18 converts to ether.
-    uint256 public rate;
-
-    // Amount of wei raised.
-    uint256 public weiRaised;
+    // Mapping of Tokens (aka photo) addresses
+    mapping(PixToken => uint256) private prices;
 
     /** Create event to log token purchase with 4 parameters:
-    * 1) Who paid for the tokens
-    * 2) Who got the tokens
-    * 3) Number of weis paid for purchase
-    * 4) Amount of tokens purchased
+    * 1) Who paid for the tokens (buyer)
+    * 2) Who got the tokens (buyer)
+    * 3) Number of weis paid for purchase (price)
+    * 4) Amount of tokens purchased = 1
     */
-    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+    event TokenPurchase(address indexed purchaser, address indexed buyer, uint256 value, uint8 amount);
 
     /** Create publicly accessible constructor function with 3 parameters:
     * 1) Rate of how many token units a buyer gets per wei
@@ -43,16 +41,14 @@ contract MintedCrowdsale {
     * 3) Address of your custom token being sold
     * Function modifiers are incredibly useful and effective. Make sure to use the right ones for each Solidity function you write.
     */
-    constructor(uint256 _rate, address _wallet, PixToken _token) public {
+    constructor(address _wallet, PixToken _token) public {
         // Set conditions with require statements to make sure the rate is a positive number and the addresses are non-zero.
-        require(_rate > 0);
         require(_wallet != address(0));
         require(_token != address(0));
 
         // Set inputs as defined state variables
-        rate = _rate;
         wallet = _wallet;
-        token = _token;
+        token.push(_token);
     }    
 
     // THIS PORTION IS FOR THE CONTRACT'S EXTERNAL INTERFACE.
@@ -66,58 +62,60 @@ contract MintedCrowdsale {
         buyTokens(msg.sender);
     }
 
-    // Create the function used for token purchase with one parameter for the address receiving the token purchase.
-    // Don't forget your modifiers.
-    function buyTokens(address _beneficiary) public payable {
+    // Create the function used for token (photo)purchase with one parameter for the address receiving the token purchase
+    // and the token that's being minted(copied) with a Bought status
+    function buyTokens(address _buyer, PixToken _token) public payable {
 
         // Define a uint256 variable that is equal to the number of wei sent with the message.
         uint256 weiAmount = msg.value;
 
+        // Check if the buyer is sending the correct price amount
+        require(weiAmount = prices[_token]);
+
         // Call function that validates an incoming purchase with two parameters, receiver and number of wei sent.
-        _preValidatePurchase(_beneficiary, weiAmount);
+        _preValidatePurchase(_buyer, weiAmount);
 
-        // Calculate token amount to be created and define it as type uint256.
-        uint256 tokens = _getTokenAmount(weiAmount);
-
-        // Update amount of funds raised.
-        weiRaised = weiRaised.add(weiAmount);
+        // Mint one token 
+        uint8 tokenAmmount = 1;
 
         // Call function that processes a purchase.
-        _processPurchase(_beneficiary, tokens);
+        _processPurchase(_buyer, tokenAmmount, _token);
 
         // Raise the event associated with a token purchase.
-        emit TokenPurchase(msg.sender, _beneficiary, weiAmount, tokens);
+        emit TokenPurchase(msg.sender, _buyer, weiAmount, tokenAmmount);
     
         // Call function that stores ETH from purchases into a wallet address.
         _forwardFunds();
     }
 
+    // Upload photo address to map and array for storing and tracking purposes
+    // called by the owner of the contract through the front end website
+    function _uploadPhoto(PixToken _tokenAddress, uint256 _price) onlyOwner external {
+        //add address to array and map with price
+        require(_price != 0);
+        token.push(_tokenAddress);
+        prices[_tokenAddress] = _price;
+    }
+
     // THIS PORTION IS FOR THE CONTRACT'S INTERNAL INTERFACE.
     // Remember, the following functions are for the contract's internal interface.
     
-    // Create function that validates an incoming purchase with two parameters: beneficiary's address and value of wei.
-    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-        // Set conditions to make sure the beneficiary's address and the value of wei involved in purchase are non-zero.
-        require(_beneficiary != address(0));
+    // Create function that validates an incoming purchase with two parameters: buyer's address and value of wei.
+    function _preValidatePurchase(address _buyer, uint256 _weiAmount) internal {
+        // Set conditions to make sure the buyer's address and the value of wei involved in purchase are non-zero.
+        require(_buyer != address(0));
         require(_weiAmount != 0);
     }
 
-    // Create function that delivers the purchased tokens with two parameters: beneficiary's address and number of tokens.
-    function _deliverTokens(address _beneficiary, uint256 _tokenAmount) internal {
+    // Create function that delivers the purchased tokens with two parameters: buyer's address and number of tokens.
+    function _deliverTokens(address _buyer, uint8 _tokenAmount, PixToken _token) internal {
         // Set condition that requires contract to mint your custom token with the mint method inherited from your MintableToken contract.
-        require(PixToken(token).mint(_beneficiary, _tokenAmount));
-  }
-
-    // Create function that executes the deliver function when a purchase has been processed with two parameters: beneficiary's address and number of tokens.
-    function _processPurchase(address _beneficiary, uint256 _tokenAmount) internal {
-        _deliverTokens(_beneficiary, _tokenAmount);
+        require(PixToken(_token).mint(_buyer, _tokenAmount));
     }
 
-    // Create function to convert purchase value in wei into tokens with one parameter: value in wei.
-    // Write the function so that it returns the number of tokens (value in wei multiplied by defined rate).
-    // Multiplication can be done as a method.
-    function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
-        return _weiAmount.mul(rate);
+    // Create function that executes the deliver function when a purchase has been processed with two parameters: buyer's address and number of tokens.
+    function _processPurchase(address _buyer, uint8 _tokenAmount, PixToken _token) internal {
+        _deliverTokens(_buyer, _tokenAmount, _token);
     }
 
     // Create function to store ETH from purchases into a wallet address.
